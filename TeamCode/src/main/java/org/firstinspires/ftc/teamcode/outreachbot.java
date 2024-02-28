bcb8d07ce27b73968c844a6c948a8b8cfb51de22
avocadoSoSilly <151409952+avocadoSoSilly@users.noreply.github.com>
2024-02-28 17:16:12 -0500

    change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/outreachbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/outreachbot.java
@@ -1,14 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
-public class outreachbot {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-
+@TeleOp
+public class EyeballsTeleop extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-
-
-
-
-
-
-}
+        //initializing hardware
